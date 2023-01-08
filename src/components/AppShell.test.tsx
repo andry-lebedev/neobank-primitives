69dd828e51b57c938ca34d575a845b961b85231f
Andry Lebedev <32692947+andry-lebedev@users.noreply.github.com>
2023-01-08 07:28:40 +0400

    codex-build: Task 19 — responsive app shell (sidebar/tabs/topbar, centered column)

---
+++ src/components/AppShell.test.tsx
@@ -0,0 +1,24 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
+import { screen } from '@testing-library/react'
+import { Home } from 'lucide-react'
+import { renderWithProviders } from '@/test/utils'
+import { setSourceOverride } from '@/data'
+import { AppShell, type NavItem } from './AppShell'
+
+beforeEach(() => vi.stubEnv('VITE_API_TOKEN', ''))
+afterEach(() => { setSourceOverride(null); vi.unstubAllEnvs() })
+
+const nav: NavItem[] = [
+  { id: 'home', route: '/', navIcon: Home, navLabel: 'Home', end: true },
+]
+
+describe('AppShell', () => {
+  it('renders brand, nav, explainer toggle and demo banner', async () => {
+    renderWithProviders(<AppShell nav={nav}><p>page body</p></AppShell>)
+    expect(await screen.findByText('page body')).toBeInTheDocument()
+    expect(screen.getAllByText('Swipelux').length).toBeGreaterThan(0)
+    expect(screen.getAllByRole('link', { name: /home/i }).length).toBeGreaterThan(0)
+    expect(screen.getByRole('button', { name: /how it works/i })).toBeInTheDocument()
+    expect(screen.getByText(/demo data/i)).toBeInTheDocument()
+  })
+})
