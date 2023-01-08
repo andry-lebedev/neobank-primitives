7f39167f15752c8a837edfc3bc72dd3719ab30b0
Andry Lebedev <32692947+andry-lebedev@users.noreply.github.com>
2023-01-08 06:27:40 +0400

    codex-build: Task 6 — explainer flows + event mapping

---
+++ src/explainers.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from 'vitest'
+import { explainers, EVENT_FLOW, stepsDone, type FlowKey } from './explainers'
+import type { ActionEventType } from './lib/events'
+
+const ALL_EVENTS: ActionEventType[] = [
+  'customer.created', 'kyc.started', 'kyc.approved', 'wallet.provisioned',
+  'account.issued', 'payout.quoted', 'payout.created', 'p2p.created',
+  'topup.created', 'transfer.updated', 'mode.changed',
+]
+
+describe('explainers', () => {
+  it('every action event maps to a flow or an explicit null', () => {
+    for (const type of ALL_EVENTS) {
+      expect(EVENT_FLOW[type], `missing mapping for ${type}`).not.toBeUndefined()
+    }
+  })
+
+  it('every mapped flow has content with at least 2 steps and a docs link', () => {
+    const flows = new Set(Object.values(EVENT_FLOW).filter((f): f is FlowKey => f !== null))
+    for (const key of flows) {
+      const flow = explainers[key]
+      expect(flow.steps.length).toBeGreaterThanOrEqual(2)
+      expect(flow.docsUrl).toMatch(/^https:\/\//)
+    }
+  })
+
+  it('stepsDone tracks transfer lifecycle', () => {
+    expect(stepsDone('payout', 'pending')).toBe(2)
+    expect(stepsDone('payout', 'in_progress')).toBe(3)
+    expect(stepsDone('payout', 'completed')).toBe(4)
+    expect(stepsDone('payout', 'failed')).toBe(-1)
+  })
+})
---
+++ src/explainers.ts
@@ -0,0 +1,105 @@
+import type { ActionEventType } from './lib/events'
+import type { TransferState } from './data/types'
+
+// "How it works" narration — product-level, per flow. Tailorable: a client AI
+// may rewrite titles/details to match the client's own wording. Keyed off
+// action events so it works identically in demo and live mode.
+
+export type FlowKey = 'onboarding' | 'payout' | 'p2p' | 'topup' | 'deposit'
+
+export interface ExplainerStep {
+  id: string
+  title: string
+  detail: string
+}
+
+export interface ExplainerFlow {
+  key: FlowKey
+  title: string
+  subtitle: string
+  docsUrl: string
+  steps: ExplainerStep[]
+}
+
+export const explainers: Record<FlowKey, ExplainerFlow> = {
+  onboarding: {
+    key: 'onboarding',
+    title: 'Behind onboarding',
+    subtitle: 'Customer → KYC → wallet → bank account',
+    docsUrl: 'https://docs.swipelux.com',
+    steps: [
+      { id: 'customer', title: 'Customer created', detail: 'A compliance-ready customer record — Swipelux is the regulated entity, you are not.' },
+      { id: 'kyc', title: 'KYC verified', detail: 'Identity verification runs through Swipelux; no KYC vendor contract needed on your side.' },
+      { id: 'wallet', title: 'Wallet provisioned', detail: 'A stablecoin wallet on Polygon, custodied by Swipelux.' },
+      { id: 'account', title: 'Bank account issued', detail: 'A named virtual IBAN that tops up the wallet automatically when money arrives.' },
+    ],
+  },
+  payout: {
+    key: 'payout',
+    title: 'Behind a bank payout',
+    subtitle: '4 steps behind one API call',
+    docsUrl: 'https://docs.swipelux.com',
+    steps: [
+      { id: 'quote', title: 'Quote locked', detail: 'Stablecoin→fiat rate fixed before you commit — you never hold FX risk.' },
+      { id: 'screen', title: 'Compliance screened', detail: 'Recipient and transaction checked automatically (AML / sanctions).' },
+      { id: 'convert', title: 'Stablecoin converted', detail: 'USDC sold, fiat settled on the Swipelux ledger.' },
+      { id: 'send', title: 'Bank payout sent', detail: 'Funds leave via SEPA/SWIFT — typically same day.' },
+    ],
+  },
+  p2p: {
+    key: 'p2p',
+    title: 'Behind a wallet transfer',
+    subtitle: 'Instant, on-ledger',
+    docsUrl: 'https://docs.swipelux.com',
+    steps: [
+      { id: 'screen', title: 'Compliance screened', detail: 'Both sides checked automatically before funds move.' },
+      { id: 'settle', title: 'Settled on-chain', detail: 'Stablecoins move wallet-to-wallet in seconds, 24/7 — no banking hours.' },
+    ],
+  },
+  topup: {
+    key: 'topup',
+    title: 'Behind a deposit',
+    subtitle: 'Fiat in, stablecoin balance out',
+    docsUrl: 'https://docs.swipelux.com',
+    steps: [
+      { id: 'receive', title: 'Funds received', detail: 'Money arrives at the virtual IBAN (or on-chain to the wallet address).' },
+      { id: 'credit', title: 'Balance credited', detail: 'Converted and credited to the customer wallet automatically — webhook fired to your backend.' },
+    ],
+  },
+  deposit: {
+    key: 'deposit',
+    title: 'Behind a deposit',
+    subtitle: 'Fiat in, stablecoin balance out',
+    docsUrl: 'https://docs.swipelux.com',
+    steps: [
+      { id: 'receive', title: 'Funds received', detail: 'Money arrives at the virtual IBAN (or on-chain to the wallet address).' },
+      { id: 'credit', title: 'Balance credited', detail: 'Converted and credited to the customer wallet automatically — webhook fired to your backend.' },
+    ],
+  },
+}
+
+// Which flow narrates which event. null = event drives no narration directly
+// (transfer.updated advances the already-open flow; mode.changed is chrome).
+export const EVENT_FLOW: Record<ActionEventType, FlowKey | null> = {
+  'customer.created': 'onboarding',
+  'kyc.started': 'onboarding',
+  'kyc.approved': 'onboarding',
+  'wallet.provisioned': 'onboarding',
+  'account.issued': 'onboarding',
+  'payout.quoted': 'payout',
+  'payout.created': 'payout',
+  'p2p.created': 'p2p',
+  'topup.created': 'topup',
+  'transfer.updated': null,
+  'mode.changed': null,
+}
+
+// How many steps of a flow are done for a given transfer state.
+// -1 signals failure (drawer renders the active step in red).
+export function stepsDone(flow: FlowKey, state: TransferState): number {
+  if (state === 'failed') return -1
+  const total = explainers[flow].steps.length
+  if (state === 'completed') return total
+  if (state === 'in_progress') return total - 1
+  return total - 2 // pending / awaiting_funds: first checks done, conversion in flight
+}
